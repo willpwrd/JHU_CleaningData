5d735b6806e5bbef3851e4e88d6fde699074242e
William Elliott <welliott18@gmail.com>
2014-04-27 18:26:47 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+#PART1
 #Read in features
 feature_data = read.table("features.txt", sep= " ", col.names = c("id", "name"))
 
@@ -12,8 +13,16 @@ stds <- feature_data[std_ind,]
 #Merge the data
 merged_data <- rbind(means,stds)
 
+#PART 2
 #Combine test/train data
 test_X <- read.table("test/X_test.txt")
 train_X <- read.table("train/X_train.txt")
 
 combined_data <- rbind(test_X, train_X)
+
+#PART 3
+#Descriptive activity names
+desc_names <- read.table("activity_labels.txt", sep = " ", col.names = c("id", "activityName"))
+
+#Give combined data descriptive names
+combined_data <- merge(combined_data, desc_names, by = "id")
